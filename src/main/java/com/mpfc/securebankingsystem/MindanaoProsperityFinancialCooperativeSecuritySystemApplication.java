29d974bdd840308789c86336dd4f644d0d704590
prince <ongp748@gmail.com>
2025-11-08 18:12:35 +0800

    feat: initial project setup for mpfc secure banking system demo

---
+++ src/main/java/com/mpfc/securebankingsystem/MindanaoProsperityFinancialCooperativeSecuritySystemApplication.java
@@ -0,0 +1,13 @@
+package com.mpfc.securebankingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindanaoProsperityFinancialCooperativeSecuritySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindanaoProsperityFinancialCooperativeSecuritySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mpfc/securebankingsystem/MindanaoProsperityFinancialCooperativeSecuritySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpfc.securebankingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindanaoProsperityFinancialCooperativeSecuritySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
